6a97df9f5709b18c40a11bf995e79eea647624e5
Aydin <aydinbg@gmail.com>
2013-09-03 18:07:20 +0200

    add index

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,12 @@ GeneralForsamling::Application.routes.draw do
   end
 end
 
-  get "welcome/index"
+  get "companies/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'companies#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
