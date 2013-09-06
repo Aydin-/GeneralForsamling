9f609487109a167d097e2652bfa966e1bc3af164
Aydin <aydinbg@gmail.com>
2013-09-06 13:51:39 +0200

    some validation & style changes

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,2 +1,6 @@
 class Company < ActiveRecord::Base
+#	if action= :edit do
+	validates :email, length: {:on => :update, minimum: 5} 
+	validates :contact_name, length: {:on => :update, minimum: 5 }
+#end
 end
